d1fa2c1dbdddb6c7fcd9ef53955fd62b5678e9ba
Miriam S <mimirsjo@gmail.com>
2019-03-04 20:57:25 -0600

    added code

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,71 @@
-print("hi world!")
+# Dependencies
+import os
+import csv
+
+# Set paths for resource and output files
+budget_path = os.path.join('Resources', 'budget_data.csv')
+output_path = os.path.join('Output','pybank_results.txt')
+
+# Create empty lists for month and revenue data
+months = []
+revenue = []
+revenue_changes = []
+
+# Create variables and set initial values
+total_revenue = 0
+previous_revenue = 0
+revenue_change = 0
+max_increase = 0
+max_decrease = 0
+
+# Open and read csv, skipping header
+with open(budget_path, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    header = next(csvreader)
+
+    # Add data to months and revenue lists
+    for row in csvreader:
+        months.append(row[0])
+        revenue.append(int(row[1]))
+        
+        # Build out revenue change list
+        revenue_change = int(row[1]) - previous_revenue
+        revenue_changes.append(revenue_change)
+
+# Count total months
+total_months = len(months)
+# print(total_months)
+
+# Calculate average_change
+average_change = round(sum(revenue_changes) / len(revenue_changes))
+# print(average_change)
+
+# Loop through revenue to calculate total revenue
+for i in range(len(revenue)):
+    # Add each revenue to total revenue
+    total_revenue = total_revenue + revenue[i]
+# print(total_revenue)
+
+# Loop through revenue_changes to find greatest increase/decrease
+    if revenue_changes[i] >= max_increase:
+        max_increase = revenue_changes[i]
+        max_increase_month = months[i]
+    elif revenue_changes[i] <= max_decrease:
+        max_decrease = revenue_changes[i]
+        max_decrease_month = months[i]
+# print(max_increase)
+# print(max_decrease)
+
+# opens the output destination in write mode and prints the summary
+with open(output_path, 'w') as writefile:
+    writefile.writelines('Financial Analysis\n')
+    writefile.writelines('----------------------------' + '\n')
+    writefile.writelines('Total Months: ' + str(total_months) + '\n')
+    writefile.writelines('Total Revenue: $' + str(total_revenue) + '\n')
+    writefile.writelines('Average Revenue Change: $' + str(average_change) + '\n')
+    writefile.writelines('Greatest Increase in Revenue: ' + max_increase_month + ' ($' + str(max_increase) + ')'+ '\n')
+    writefile.writelines('Greatest Decrease in Revenue: ' + max_decrease_month + ' ($' + str(max_decrease) + ')')
+
+#opens the output file in r mode and prints to terminal
+with open(output_path, 'r') as readfile:
+    print(readfile.read())
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,1 +1,76 @@
-print("hi world!")
+# Dependencies
+import os
+import csv
+
+# Set paths for resource and output files
+election_path = os.path.join('Resources', 'election_data.csv')
+output_path = os.path.join('Output', 'election_results.txt')
+
+# Create a dictionary to be used for candidate name and vote count
+election = {}
+
+# Set variable total votes and initial value to zero
+total_votes = 0
+
+# Open data file
+with open(election_path, 'r') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
+
+    # Skip header
+    header = next(csvreader)
+
+    # Create a dictionary from file using column 3 as keys, using each name only once.
+    # Count votes for each candidate as entries
+    # Keep a total vote count by counting up 1 for each loop (# of rows w/o header)
+    for row in csvreader:
+        total_votes = total_votes + 1
+        if row[2] in election.keys():
+            election[row[2]] = election[row[2]] + 1
+        else:
+            election[row[2]] = 1
+ 
+# Create empty list for candidates and their vote count
+candidates = []
+votes = []
+
+# Create lists to hold dictionary keys (candidates) and values (votes) 
+for candidate, vote in election.items():
+    candidates.append(candidate)
+    votes.append(vote)
+
+# Create vote percent list
+vote_percent = []
+for n in votes:
+    vote_percent.append(round(n/total_votes*100, 1))
+
+# Zip candidates, votes, vote_percent into tuples
+clean_data = list(zip(candidates, votes, vote_percent))
+
+# Create winner_list for the winners
+winner_list = []
+
+for name in clean_data:
+    if max(votes) == name[1]:
+        winner_list.append(name[0])
+
+# Make winner_list a str with the first entry
+winner = winner_list[0]
+
+# Run if there is a tie to add winners into a string separated by commas
+if len(winner_list) > 1:
+    for w in range(1, len(winner_list)):
+        winner = winner + ", " + winner_list[w]
+
+# Print to file
+
+
+with open(output_path, 'w') as txtfile:
+    txtfile.writelines('Election Results \n------------------------- \nTotal Votes: ' + str(total_votes) + 
+      '\n-------------------------\n')
+    for entry in clean_data:
+        txtfile.writelines(entry[0] + ": " + str(entry[2]) +'%  (' + str(entry[1]) + ')\n')
+    txtfile.writelines('------------------------- \nWinner: ' + winner + '\n-------------------------')
+
+#prints file to terminal
+with open(output_path, 'r') as readfile:
+    print(readfile.read())
